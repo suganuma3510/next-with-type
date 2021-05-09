0da271f50c99f0e84a6d5ca73b8f22bb2701c511
suganuma3510 <suganuma3510@gmail.com>
2021-05-09 13:49:01 +0900

    add：TypeScriptを導入しjsファイルをtsxファイルに変更

---
+++ app/src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import 'styles/globals.css';
+
+import { NextPage } from 'next';
+import { AppProps } from 'next/dist/next-server/lib/router/router';
+
+const MyApp: NextPage<AppProps> = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
